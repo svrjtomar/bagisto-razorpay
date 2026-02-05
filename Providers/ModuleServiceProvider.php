6207dee6eec81808ae0c44ddb1478af855e9627b
Shiv Raj singh <107348329+svrjtomar@users.noreply.github.com>
2026-02-05 22:53:04 +0530

    Add files via upload

---
+++ Providers/ModuleServiceProvider.php
@@ -0,0 +1,8 @@
+namespace SleepyBear\Razorpay\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+    protected $models = [];
+}
